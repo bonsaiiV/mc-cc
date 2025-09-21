8ab55f556f2eba88735cafdbb6b0387babccc40e
Bonsaiiv <dominik.riemer@udo.edu>
2025-09-21 21:40:51 +0200

    mine: check for lava

--- mine.lua
+++ mine.lua
@@ -2,9 +2,16 @@ local target = tonumber(arg[1])
 local dist = 0
 
 while(target > dist) do
+    local has_block, data = turtle.inspect()
+    if has_block then
+        if (data.name == "minecraft:lava") then
+            break
+        end
+        print(data.name)
+    end
     turtle.dig()
     turtle.forward()
-    turtle.digDown()
+    turtle.digUp()
     dist = dist+1
 end
 while (dist > 0) do
