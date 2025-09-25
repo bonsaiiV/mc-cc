157d5680402a8f92df04f40d1d4fc60e18e51521
Bonsaiiv <dominik.riemer@udo.edu>
2025-09-25 16:51:38 +0200

    mine: improve lava detection

--- mine.lua
+++ mine.lua
@@ -2,6 +2,7 @@ local target = tonumber(arg[1])
 local dist = 0
 
 while(target > dist) do
+    turtle.dig()
     local has_block, data = turtle.inspect()
     if has_block then
         if (data.name == "minecraft:lava") then
@@ -9,7 +10,6 @@ while(target > dist) do
         end
         print(data.name)
     end
-    turtle.dig()
     turtle.forward()
     turtle.digUp()
     dist = dist+1
