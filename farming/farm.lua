e14ad7b436f6e027890bf4a72d54cbaf4abd2702
Bonsaiiv <dominik.riemer@udo.edu>
2025-11-15 21:07:54 +0100

    farming: refuel

--- farming/farm.lua
+++ farming/farm.lua
@@ -11,13 +11,48 @@ end
 
 local function wait_for_regrow()
     os.sleep(300)
+end
+
+local function c_refuel()
+        local has_block, data = turtle.inspect()
+            if (has_block and data.name == "minecraft:lava_cauldron") then
+            turtle.place()
+            turtle.refuel()
+        end
+end
+
+local function refuel()
     turtle.forward()
+    local has_block, data
+    if turtle.getFuelLevel() > 19000 then
+        goto refuel_end
+    end
+    if find_in_inventory("minecraft:lava_bucket") then
+        turtle.refuel()
+        goto refuel_end
+    end
+
+    has_block, data = turtle.inspectDown()
+    if not has_block then goto  refuel_end end
+    find_in_inventory("minecraft:bucket")
+    if data.name == "minecraft:red_concrete" then
+        turtle.turnLeft()
+        c_refuel()
+        turtle.turnRight()
+    elseif data.name == "minecraft:blue_concrete" then
+        turtle.turnRight()
+        c_refuel()
+        turtle.turnLeft()
+    end
+    ::refuel_end::
+    turtle.select(1)
 end
 
 local control_blocks = {}
-control_blocks["minecraft:red_concrete"] = leftCorner
-control_blocks["minecraft:blue_concrete"] = rightCorner
+control_blocks["minecraft:red_concrete"] = turtle.turnLeft
+control_blocks["minecraft:blue_concrete"] = turtle.turnRight
 control_blocks["minecraft:black_concrete"] = wait_for_regrow
+control_blocks["minecraft:yellow_concrete"] = refuel
 
 local crops = {}
 crops["minecraft:nether_wart"] = {max_age = 3, seed_name = "minecraft:nether_wart"}
@@ -26,12 +61,10 @@ local mineable_blocks = {"minecraft:pointed_dripstone"}
 
 local function step()
     local has_block_down, data_down = turtle.inspectDown()
-    local has_block_fw, data_fw = turtle.inspect()
     if has_block_down then
         local c_fun = control_blocks[data_down.name]
         if c_fun then
             c_fun()
-            return
         end
 
         crop = crops[data_down.name]
@@ -56,6 +89,7 @@ local function step()
             end
         end
     end
+    local has_block_fw, data_fw = turtle.inspect()
     if has_block_fw then
         for _, name in pairs(mineable_blocks) do
             if name == data_fw.name then
